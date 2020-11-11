4f8e46ab11d8b3ce1a1b4e22b218c8dacb0a8cb4
Kaustav Das Modak <kaustav.dasmodak@gmail.com>
2020-11-11 09:54:32 +0530

    Write full example
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@gmail.com>

--- index.js
+++ index.js
@@ -25,8 +25,13 @@ const App = {
 
         try {
             await this.login()
+            await this.client.startClient()
+            console.log('Logged in')
+            await this.sync()
+            console.log('Sync started')
             await this.join()
             await this.send()
+            await this.recv()
             await this.stop()
         } catch (err) {
             console.error(`${err.errcode} - ${err.httpStatus}`)
@@ -43,16 +48,44 @@ const App = {
         })
     },
 
+    sync: function () {
+        return new Promise((resolve, reject) => {
+            this.client.on('sync', (state, prevState, res) => {
+                if (state === 'PREPARED') {
+                    resolve()
+                } else {
+                    reject(res)
+                }
+            })
+        })
+    },
+
     join: function join() {
         // Use client.joinRoom(roomAlias, opts)
+        return this.client.joinRoom(roomAlias)
     },
 
     send: function sendGreeting() {
         // Use client.sendMessage(roomAlias, content)
+        return this.client.sendMessage(roomAlias, {
+            body: 'OHAI!',
+            msgtype: 'm.room.message'
+        })
     },
 
     recv: function recvGreeting() {
-        // Use client.on('Room.timeline', (e) => {})
+        // Use client.on('Room.timeline, (e, room, start) => {})
+        return new Promise((resolve, reject) => {
+            this.client.on("Room.timeline", (e, room, toStartOfTimeline) => {
+                const event = e.event
+                if (e.getType() === "m.room.message" && event.content.body.toLowerCase().startsWith('hello')) {
+                    console.log(`Received *hello* from ${event.sender}`)
+                    resolve()
+                } else {
+                    console.log(`[${e.getType()}] ${event.sender}: ${event.content.body}`)
+                }
+            })
+        })
     },
 
     stop: function stop() {
